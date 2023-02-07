71528600404c85e9eb1d8833971515eb33812c55
erp_user <erp_user@dexciss>
2023-02-07 16:30:34 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Book House
+
+All books and navels are available in this house.
+
+#### License
+
+MIT
---
+++ library_management/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Book House",
+			"type": "module",
+			"label": _("Book House")
+		}
+	]
