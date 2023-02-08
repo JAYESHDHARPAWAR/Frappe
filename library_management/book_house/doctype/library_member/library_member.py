424ff995ea01f44a37418e90d3f2b030cae9b503
JAYESHDHARPAWAR <jayeshdharpawar94@gmail.com>
2023-02-08 18:19:16 +0530

    feat:added new doctype

---
+++ library_management/book_house/doctype/library_member/library_member.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Shubham Kuthe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Librarymember(Document):
+	pass
