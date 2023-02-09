7c65aa0ee3183672450b290631825f1e394f9178
JAYESHDHARPAWAR <jayeshdharpawar94@gmail.com>
2023-02-09 17:07:55 +0530

    feat:validation Membership, dt library setting, dt library transaction, validation library setting

--- library_management/book_house/doctype/library_membership/library_membership.py
+++ library_management/book_house/doctype/library_membership/library_membership.py
@@ -1,8 +1,30 @@
 # Copyright (c) 2023, Shubham Kuthe and contributors
 # For license information, please see license.txt
-
-# import frappe
+'''
+import frappe
 from frappe.model.document import Document
 
 class LibraryMembership(Document):
 	pass
+'''
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
+
+
+class LibraryMembership(Document):
+    # check before submitting this document
+    def before_submit(self):
+        exists = frappe.db.exists(
+            "Library Membership",
+            {   
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                # check if the membership's end date is later than this membership's start date
+                "to_date": (">", self.from_date),
+            },
+        )
+        if exists:
+            #var = "library_member"
+            frappe.throw("There is an active membership for this member {}".format(self.library_member))
